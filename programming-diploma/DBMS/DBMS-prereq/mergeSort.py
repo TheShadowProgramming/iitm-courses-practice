3ba2e98f611b4ec692f0c1c81613d9e5e4851e1a
Monish Mishra <monishmprogram@gmail.com>
2024-09-22 23:37:23 +0530

    merge sort half done

--- programming-diploma/DBMS/DBMS-prereq/mergeSort.py
+++ programming-diploma/DBMS/DBMS-prereq/mergeSort.py
@@ -5,13 +5,18 @@
     then we sort the arrays of length 2 linearly and add the arrays recursively, and then sort the newlyformed array again
 '''
 
-def mergeSort (unsortedArray):
-    return None
-
+unsortedArray = input('enter the unsorted array in form of string here');
 
 def sortTwoElements(array):
-    if array[0] < array[1]:
+    if (array[0] < array[1]) or (array[0] == array[1]):
         return array;
-    elif array[0] > array[1]:
+    elif array[0] > array[1]: # works for numbers only, well this sorting algorithm only makes sense under the sorting context
         array[0] = array[0] + array[1];
         array[1] = array[0] - array[1];
+        array[0] = array[0] - array[1];
+
+def mergeSort (unsortedArray):
+    if len(unsortedArray) == 2:
+        return sortTwoElements(unsortedArray)
+    middleIndex = len(unsortedArray) // 2
+    return None;
